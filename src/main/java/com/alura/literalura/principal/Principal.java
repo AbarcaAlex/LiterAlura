f22fa48c3e086a2c23841283a7cea1fcd6fc1002
Alexis Abarca <alexisabarcaz@outlook.com>
2024-06-13 11:30:16 -0600

    se creo el menu a imprimir

--- src/main/java/com/alura/literalura/principal/Principal.java
+++ src/main/java/com/alura/literalura/principal/Principal.java
@@ -4,6 +4,17 @@ public class Principal {
 
     public static void mostrarMenu(){
 
+        System.out.printf("""
+        ----------------------
+        Elija la opción a traves de su numero:
+        1 - Buscar libro por titulo
+        2 - Listar libros registrados
+        3 - Listar autores registrados
+        4 - Listar autores vivos en un determinado año
+        5 - Listar libros por idioma
+        0 - Salir
+        """);
+
         
     }
 }
